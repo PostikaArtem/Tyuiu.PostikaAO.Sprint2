dbb04d71bd3ba42172b09fe2052547891602a67d
PostikaArtem <Tyuiu.PostikaAO@outlook.com>
2023-10-15 18:21:19 +0500

    Task4

---
+++ Tyuiu.PostikaAO.Task4.V24.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PostikaAO.Sprint2.Task4.V24.Lib;
+
+namespace Tyuiu.PostikaAO.Sprint2.Task4.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondiction1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 5;
+            double res = ds.Calculate(x, y);
+            double wait = 4.5;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondiction2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double y = -34;
+            double res = ds.Calculate(x, y);
+            double wait = 99.487;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PostikaAO.Task4.V24/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PostikaAO.Sprint2.Task4.V24.Lib;
+
+namespace Tyuiu.PostikaAO.Sprint2.Task4.V24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Постика А. О. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант 24                                                              *");
+            Console.WriteLine("* Выполнил: Постика Артём Олегович | ИИПб-23-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с              *");
+            Console.WriteLine("* использованием тернарного оператора.                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Введите значение переменной X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.Calculate(x, y);
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
